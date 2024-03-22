8f11a5a093053be4f54d504c5e8a7b3aeaf5c8b9
Yuliia Kharchenko <uliyakharchenko@gmail.com>
2024-03-22 10:50:42 +0000

    Lab 9 added

---
+++ Week09/useFib.py
@@ -0,0 +1,8 @@
+# this program prompts the user for a number and 
+# prints out the fibonacci sequence of that many numbers
+# I made a mistake with 
+
+import myfunctions
+
+nTimes = int(input('how many:'))
+print(myfunctions.fibonacci(nTimes))
